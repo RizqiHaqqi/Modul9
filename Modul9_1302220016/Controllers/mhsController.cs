d9833d50043a2cdff11089dbf42975e265fb8dad
Belvanaufal <qhikizaalicia@gmail.com>
2024-05-02 10:56:05 +0700

    API Mahasiswa

---
+++ Modul9_1302220016/Controllers/mhsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modul9_1302220016.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class mhsController : Controller
+    {
+        private static List<string> Course = new List<string>
+        {
+            "KPL", "BD", "PBO"
+        };
+        private static List<mhs> Listmhs = new List<mhs>
+        {
+            new mhs("rizqi", "1302220016", Course, 2022),
+            new mhs("ryan", "1302220010", Course, 2022),
+            new mhs("rizal", "1302220020", Course, 2022),
+    };
+        [HttpGet]
+        public IEnumerable<mhs> Get()
+        {
+            return Listmhs;
+        }
+        [HttpGet("{id}")]
+        public mhs get(int id)
+        {
+            return Listmhs[id];
+        }
+        [HttpPost]
+        public void post(mhs mhs)
+        {
+            Listmhs.Add(mhs);
+        }
+        [HttpDelete("{id}")]
+        public void delete(int id)
+        {
+            Listmhs.RemoveAt(id);
+        }
+    }
+}
--- Modul9_1302220016/mhs.cs
+++ Modul9_1302220016/mhs.cs
@@ -6,6 +6,12 @@
         public string NIM { get; set; }
         public List<string> Course { get; set; }
         public int Year { get; set; }
-            
+        public mhs(string Nama, string NIM, List<string> Course, int Year)
+        {
+            this.Nama = Nama;
+            this.NIM = NIM;
+            this.Course = Course;
+            this.Year = Year;
+        }  
     }
 }

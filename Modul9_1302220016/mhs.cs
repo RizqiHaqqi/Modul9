8f3fb2e764b0480740659cf7069bef1663880d04
Belvanaufal <qhikizaalicia@gmail.com>
2024-05-02 10:16:19 +0700

    Add project files.

---
+++ Modul9_1302220016/mhs.cs
@@ -0,0 +1,11 @@
+﻿namespace Modul9_1302220016
+{
+    public class mhs
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+            
+    }
+}
